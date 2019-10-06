e54348279c0370a49b7427f21b353011d28d87de
Michael Pu <michael.pu123@gmail.com>
2019-10-06 02:07:02 -0400

    Update packages for backend

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,12 @@
-let express = require("express"),
-    mongoose = require("mongoose");
+let express = require('express');
+let mongoose = require('mongoose');
 
 let app = express();
 
-app.get("/", function(req, res) {
-    res.send("Hello, world!");
-})
+app.get('/', function (req, res) {
+    res.send('Hello, world!');
+});
 
-app.listen(3000, function() {
-    console.log("The team hub server has started");
+app.listen(3000, function () {
+    console.log('The team hub server has started');
 });
