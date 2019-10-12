cb4dce8dbc0be0233e55b927d46f3a82ba80e3a7
Clive Chan <cc@clive.io>
2019-10-12 16:36:00 -0400

    add basic api file structure

---
+++ backend/index.js
@@ -0,0 +1,6 @@
+const router = require('express').Router()
+
+router.use("/members", require("./members"))
+router.use("/teams", require("./teams"))
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const next = require('next');
+const express = require('express')
+const next = require('next')
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const nextapp = next({ dev })
+const api = require('./backend/index')
 
-app.prepare().then(() => {
-  const server = express();
+nextapp.prepare().then(() => {
+  const server = express()
 
-  server.all('*', (req, res) => {
-    return handle(req, res)
-  })
+  server.use("/api", api)
+
+  server.all('*', nextapp.getRequestHandler())
 
   server.listen(port, err => {
     if (err) throw err
