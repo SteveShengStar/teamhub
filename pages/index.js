2b47d7608d9478a2efd3e26f3705351a3599f117
Kevin Bai <kevin.c.bai0@gmail.com>
2019-11-05 01:36:11 -0500

    Updated package-lock and ui of main page

--- pages/index.js
+++ pages/index.js
@@ -6,24 +6,29 @@ import { addMember, removeMember } from '../data/reducers/membersSlice';
 
 import PageTemplate from '../components/templates/PageTemplate';
 import { SystemComponent } from '../components/atoms/SystemComponents';
+import Button from '../components/atoms/Button';
+import Card from '../components/atoms/Card';
+import Header4 from '../components/atoms/Header4';
 
 const Home = ({ members, addMember, removeMember }) => {
   const [count, setCount] = useState(0);
   return (
     <PageTemplate title="Explore">
       <SystemComponent>
-        {members.map((i) => i.id)}
-        <button
-            onClick={() => {
-                setCount(count + 1);
-                addMember({ id: count });
-            }}
-        >
-            Add State
-        </button>
-        <button onClick={() => removeMember(count - 1)}>
-            Remove Latest State
-        </button>
+        <Card width={"300px"}>
+          <Header4>Members State: {members.map((i) => i.id)}</Header4>
+          <Button mr={3}
+              onClick={() => {
+                  setCount(count + 1);
+                  addMember({ id: count });
+              }}
+          >
+              Add State
+          </Button>
+          <Button variant={"ghostNeutral"} onClick={() => removeMember(count - 1)}>
+              Remove Latest State
+          </Button>
+        </Card>
       </SystemComponent>
     </PageTemplate>
   );
