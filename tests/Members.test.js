a2233dbb6bb9e5cdab624fb4b1478324b6526a0c
mrkarezina <mrk.arezina@gmail.com>
2019-11-16 15:40:07 -0500

    Test Search

--- tests/Members.test.js
+++ tests/Members.test.js
@@ -1,19 +1,81 @@
 jest.setTimeout(40000);
 
-
 const api = require('../backend/index');
-const skills = require('../backend/data/handlers/skills');
 
 beforeAll(async (done) => {
     await api.data.init();
     done();
 });
 
+const testMemberData = {
+    name: {
+        first: 'Test',
+        last: 'User',
+        display: 'Test User'
+    },
+    bio: 'This is my bio.',
+    skills: ['MongoDB'],
+    joined: {
+        year: 2019,
+        season: 'Fall'
+    },
+    coopExp: [],
+    memberType: 'Newbie',
+    subteam: 'Software',
+    project: {
+        name: 'TeamHub',
+        subteams: ['Software']
+    },
+    email: 'test@testing.test',
+    stream: {
+        isCoop: true,
+        onStream: true,
+        coopStream: 1,
+        currentSchoolTerm: '1A'
+    },
+    age: 18,
+    birthday: {
+        month: 4,
+        day: 10
+    },
+    links: [
+        {
+            type: 'GitHub',
+            link: 'https://github.com/mchlp'
+        }
+    ]
+};
+
 
 describe('Testing Members functions', () => {
 
+    it('Test Adding Member', async () => {
 
+        const resp = await api.data.util.resWrapper(async () => {
+            return await api.data.members.add(testMemberData);
+        });
 
+        console.log(resp)
+
+        expect(resp.success).toBe(true);
+    });
+
+    it('Searching a Member', async () => {
+
+        const searchData = {
+            "query": {
+                "name": {
+                    "first": "Michael"
+                }
+            }
+        };
+
+        const resp = await api.data.util.resWrapper(async () => {
+            return await api.data.members.search(searchData);
+        });
+
+        expect(resp.success).toBe(true);
+    });
 
 
 });
