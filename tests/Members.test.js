096c8e945b0d96bc0ebaac2b7734082690a493b9
mrkarezina <mrk.arezina@gmail.com>
2019-11-27 00:30:19 -0500

    Tests for add / delete members

--- tests/Members.test.js
+++ tests/Members.test.js
@@ -1,7 +1,7 @@
-jest.setTimeout(60000);
+jest.setTimeout(30000);
 
 const api = require('../backend/index');
-const Member = require('../backend/data/schema/Member')
+const Member = require('../backend/data/schema/Member');
 
 beforeAll(async (done) => {
     await api.data.init();
@@ -55,17 +55,22 @@ const testMemberData = {
 };
 
 
-const setupMember = () => {
-    // Boiler plate for adding the member to the DB each time
+// TODO: potential conflict when many tests running at once, potentially adding 2 members before they're deleted
+
+
+// Boiler plate for adding the member to the DB before test
+const addTestMember = async () => {
+
+    return await api.data.util.resWrapper(async () => {
+        return await api.data.members.add(testMemberData);
+    });
 
 };
 
 // Drop member before / after running a test
-const dropMember = async () => {
-    return await Member.deleteMany({ "name.first": "TestUser1010" });
+const dropTestMember = async () => {
+    return await Member.deleteMany({"name.first": "TestUser1010"});
 };
-
-
 
 
 describe('Testing Members functions', () => {
@@ -73,19 +78,17 @@ describe('Testing Members functions', () => {
     it('Tests Adding Member', async () => {
 
         // Ensure the test user is deleted first
-        let resp = await dropMember();
+        let resp = await dropTestMember();
         expect(resp.ok).toBe(1);
 
-        resp = await api.data.util.resWrapper(async () => {
-            return await api.data.members.add(testMemberData);
-        });
-
+        // Test adding the member
+        resp = await addTestMember();
         expect(resp.success).toBe(true);
 
         // Find the member that was just created
         resp = await api.data.util.resWrapper(async () => {
             return await api.data.members.search({
-                    "_id": resp.body._id
+                "_id": resp.body._id
             });
         });
 
@@ -93,12 +96,40 @@ describe('Testing Members functions', () => {
         expect(resp.body[0].email).toBeTruthy();
         expect(resp.body[0].name).toBeTruthy();
 
-        await dropMember();
+        await dropTestMember();
 
     });
 
+    it('Tests deleting a members data', async () => {
+
+        let resp = await addTestMember();
+        let id = resp.body._id;
+
+        resp = await api.data.util.resWrapper(async () => {
+            return await api.data.members.delete({_id: id});
+        });
+        expect(resp.success).toBe(true);
+
+        // Try finding the inserted user
+        resp = await api.data.util.resWrapper(async () => {
+            return await api.data.members.search({
+                "_id": id
+            });
+        });
+
+        // Ensures member deleted
+        expect(resp.body).toStrictEqual([]);
+
+    });
+
+});
+
+
+describe('Updating and Searching Members', () => {
+
     it('Tests updating a members data', async () => {
 
+        // TODO: create member and update with ID
         const resp = await api.data.util.resWrapper(async () => {
             return await api.data.members.updateMember({_id: '5dd1df5bc08e041f7d7b83f1'}, {"email": "user1011@testing.ca"});
         });
@@ -109,29 +140,23 @@ describe('Testing Members functions', () => {
 
     it('Tests searching a Member', async () => {
 
+        await addTestMember();
+
         const searchData = {
-            "query": {
-                "name": {
-                    "first": "User1010"
-                }
-            }
+            "name.first": "TestUser1010"
         };
 
         const resp = await api.data.util.resWrapper(async () => {
             return await api.data.members.search(searchData);
         });
 
-        expect(resp.success).toBe(true);
-    });
-
-    it('Tests deleting a members data', async () => {
-
-        const resp = await api.data.util.resWrapper(async () => {
-            return await api.data.members.delete({_id: '5dd1df5bc08e041f7d7b83f1'});
-        });
+        console.log(resp);
 
         expect(resp.success).toBe(true);
+
+        await dropTestMember();
     });
+
 
 });
 
