f54ac346da1c9138199e29488a7d2933dbe0f694
Michael Pu <michael.pu123@gmail.com>
2019-12-27 00:07:19 -0500

    Fix bug

--- backend/data/index.js
+++ backend/data/index.js
@@ -3,12 +3,15 @@ const mongoose = require('mongoose');
 const test_env = process.env.TEAMHUB_ENV;
 const env = process.env.NODE_ENV;
 
-let config = require('./config.json');
+let config = {};
+
 if (test_env === 'testing') {
     // Different connection string required for unit tests
     config = require('./config.tests.json');
 } else if (env === 'production') {
     config.url = process.env.MONGO_URL;
+} else {
+    config = require('./config.json');
 }
 
 const data = {};
