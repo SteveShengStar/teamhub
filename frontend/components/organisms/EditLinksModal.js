c606550cab72a8d8c969e56940717c2b2c62ad13
Steven Xiong <steveshengstar@gmail.com>
2020-02-21 20:32:02 -0500

    temp

--- frontend/components/organisms/EditLinksModal.js
+++ frontend/components/organisms/EditLinksModal.js
@@ -4,14 +4,77 @@ import {SystemComponent} from '../atoms/SystemComponents';
 
 import Input from '../atoms/Input';
 import Header5 from '../atoms/Header5';
+import isURL from 'validator/lib/isURL';
 
 const CustomInput = styled(Input)`
     box-sizing: border-box;
     height: 34px;
     width: 100%;
 `;
+// Handle error display here
+// Pass flags up to the parent
+
+// How about value
+// validation happens here, so just store the value here.
+const URLField = ({headerText, name, placeholder, value, onHandleChange}) => {
+    return (
+        <>
+            <Header5>{headerText}</Header5>
+            <CustomInput 
+                name={name}
+                placeholder={placeholder}
+                variant="text"
+                value={value}
+                onChange={(evt) => onHandleChange(name, evt.target.value)} 
+            />
+            <SystemComponent></SystemComponent>
+        </>
+    )
+}
 
 const EditLinksModal = () => {
+    
+    const [formValues, setformValues] = useState({
+        facebookUrl: '',
+        linkedInUrl: '',
+        githubUrl: '',
+        websiteUrl: ''
+    });
+    const [hasError, setHasError] = setState({
+        facebookUrl: '',
+        linkedInUrl: '',
+        githubUrl: '',
+        websiteUrl: ''
+    });
+
+    const validateUrl = (errorList, fieldName, fieldValue, allowedHostsList = false) => {
+        if (!isUrl(fieldValue, {host_whitelist: allowedHostsList})) {
+            errorList[fieldName] = false;
+        } else {
+            errorList[fieldName] = true;
+        }
+    }
+
+    const handleSave = (evt) => {
+        evt.preventDefault();
+
+        const errors = {};
+        validateUrl(errors, 'facebookUrl', formValues['facebookUrl'], ['facebook.com']);
+        validateUrl(errors, 'githubUrl', formValues['githubUrl'], ['github.com']);
+        validateUrl(errors, 'linkedInUrl', formValues['linkedInUrl'], ['linkedin.com']);
+        validateUrl(errors, 'websiteUrl', formValues['websiteUrl']);
+
+        // TODO: remove error as soon as typing begins
+        setHasError(errors);
+    }
+
+    const handleChange = (name, value) => {
+        setFormValues({
+            ...formValues,
+            [name]: value
+        })
+    }
+
     return (
         <SystemComponent display="grid" 
             gridTemplateColumns={["100%", "repeat(2, 1fr)"]}
@@ -19,8 +82,14 @@ const EditLinksModal = () => {
             gridAutoRows='minmax(70px, auto)'
         >
             <SystemComponent>
-                <Header5>Personal Website</Header5>
-                <CustomInput variant="text" placeholder="Search" value={"Filler"} />
+                <URLField
+                    label="Personal Website"
+                    name="websiteUrl"
+                    placeholder="Search" 
+                    value={formValues["websiteUrl"]}
+                    errorText={"Please Enter Valid Facebook Profile Url."}
+                    onHandleChange={handleChange}
+                />
             </SystemComponent>
             <SystemComponent>
                 <Header5>Github</Header5>   
