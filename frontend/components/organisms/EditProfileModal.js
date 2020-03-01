190853bd99f6675b8e1490a68d0ce269e8e8d889
Steven Xiong <steveshengstar@gmail.com>
2020-03-01 18:55:34 -0500

    In Progress: form validation for Edit Profile Modal

--- frontend/components/organisms/EditProfileModal.js
+++ frontend/components/organisms/EditProfileModal.js
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
 import {SystemComponent} from '../atoms/SystemComponents';
+import {useState} from 'react';
 import EditSettingsModal from '../molecules/EditSettingsModal';
 import SettingsInputPair from '../molecules/AccountSettings/SettingsInputPair';
 
 import Input from '../atoms/Input';
 import Header5 from '../atoms/Header5';
+
+import {isEmail} from 'validator';
+import theme from '../theme';
+
 
 const CustomInput = styled(Input)`
     box-sizing: border-box;
@@ -12,9 +17,82 @@ const CustomInput = styled(Input)`
     width: 100%;
 `;
 
-const EditProfileModal = ({visible, handleCloseModal}) => {
-    const handleSave = () => {
+// TODO: make into a molecule
+const InputSegment = ({label, name, placeholder, value, onHandleChange, error = false, errorText = ""}) => {
+    return (
+        <>
+            <Header5>{label}</Header5>
+            <CustomInput 
+                name={name}
+                placeholder={placeholder}
+                variant="text"
+                value={value}
+                onChange={(evt) => onHandleChange(name, evt.target.value)} 
+            />
+            {error && <SystemComponent color={theme.colors.alertAction}>{errorText}</SystemComponent>}
+        </>
+    )
+}
+const SelectSegment = ({title,
+    name,
+    handleChange,
+    value,
+    options,
+    allowCustomInput = false,
+    error = false, 
+    errorText = ""}) => {
 
+        // TODO: refactor this
+        const handleOptChange = (title, val) => {
+            handleChange(name, val);
+        };
+
+        return (
+            <SystemComponent>
+                <SettingsInputPair title={title}
+                    value={value}
+                    options={options}
+                    onChange={handleOptChange} // TODO: extra parameter passed up.
+                    isMulti={false}
+                    allowCustomInput={allowCustomInput}
+                />
+                {error && <SystemComponent color={theme.colors.alertAction}>{errorText}</SystemComponent>}
+            </SystemComponent>
+        );
+}
+
+const validProgramPattern = /^[A-Za-z,'-\s]*$/;
+const EditProfileModal = ({visible, handleCloseModal}) => {
+    const [formValues, setFormValues] = useState({
+        firstName: "",
+        lastName: "",
+        birthDate: "",
+        email: "",
+        program: "",
+        term: "",
+        sequence: ""
+    });
+    const [hasError, setHasError] = useState({ // TODO: Think about different locales later
+        email: false,
+        program: false
+    });
+
+    const handleSave = () => {
+        const updatedErrorList = {...hasError};
+        updatedErrorList['email'] = 
+            !isEmail(formValues['email']);
+        //console.log(validProgramPattern.test(formValues['program'].value.trim()));
+        updatedErrorList['program'] = 
+            !validProgramPattern.test(formValues['program'].value.trim());
+
+        setHasError(updatedErrorList);
+    } 
+
+    const handleInputChange = (name, value) => {
+        if (hasError[name]) {
+            hasError[name] = false;
+        }
+        setFormValues({...formValues, [name]: value});
     }
     
     return (
@@ -30,31 +108,78 @@ const EditProfileModal = ({visible, handleCloseModal}) => {
                 gridAutoRows='minmax(70px, auto)'
             >
                 <SystemComponent>
-                    <Header5>First Name</Header5>
-                    <CustomInput variant="text" placeholder="Search" value={"Filler"} />
+                    <InputSegment
+                        label="First Name" // TODO: think about different locales and name validation 
+                        name="firstName"
+                        placeholder="First Name" 
+                        value={formValues['firstName']}
+                        onHandleChange={handleInputChange} />
                 </SystemComponent>
                 <SystemComponent>
-                    <Header5>Last Name</Header5>   
-                    <CustomInput variant="text" placeholder="Search" value={"Filler"} />
+                    <InputSegment
+                        label="Last Name" 
+                        name="lastName"
+                        placeholder="Last Name" 
+                        value={formValues['lastName']}
+                        onHandleChange={handleInputChange} />
                 </SystemComponent>
                 <SystemComponent>
-                    <Header5>BirthDate</Header5>  
-                    <CustomInput variant="text" placeholder="Search" value={"Filler"} />
-                </SystemComponent>
-                <SystemComponent>   
-                    <Header5>Email</Header5>  
-                    <CustomInput variant="text" placeholder="Search" value={"Filler"} />
-                </SystemComponent>
-
-                <SystemComponent>
-                    <SettingsInputPair title="Academic Program"></SettingsInputPair>
+                    <InputSegment
+                        label="Birth Date" 
+                        name="birthDate"
+                        placeholder="Birth Date" 
+                        value={formValues['birthDate']}
+                        onHandleChange={handleInputChange} />
                 </SystemComponent>
                 <SystemComponent>
-                    <SettingsInputPair title="School Term"></SettingsInputPair>
+                    <InputSegment
+                        label="Email" 
+                        name="email"
+                        placeholder="Email" 
+                        value={formValues['email']}
+                        onHandleChange={handleInputChange} 
+                        error={hasError['email']}
+                        errorText="Please Enter a valid Email Address" />
                 </SystemComponent>
-                <SystemComponent>
-                    <SettingsInputPair title="Work/Study Sequence"></SettingsInputPair>
-                </SystemComponent>
+                <SelectSegment title="Academic Program"
+                    name="program"
+                    value={formValues['program']}
+                    options={[
+                        {value: 'eng', label: 'Engineering'},
+                        {value: 'cs', label: 'Computer Science'},
+                        {value: 'math', label: 'Math'}
+                    ]}
+                    handleChange={handleInputChange}
+                    allowCustomInput={true}
+                    error={hasError['program']}
+                    errorText={"Please Enter Valid Program Name. All letters and these special characters { - ' , } are allowed."}
+                />
+                <SelectSegment title="School Term"
+                    name="term"
+                    value={formValues['term']}
+                    options={[
+                        {value: '1a', label: '1A'},
+                        {value: '1b', label: '1B'},
+                        {value: '2a', label: '2A'},
+                        {value: '2b', label: '2B'},
+                        {value: '3a', label: '3A'},
+                        {value: '3b', label: '3B'},
+                        {value: '4a', label: '4A'},
+                        {value: '4b', label: '4B'},
+                        {value: '0', label: 'Other'}
+                    ]}
+                    handleChange={handleInputChange}
+                />
+                <SelectSegment title="Work-Study Sequence"
+                    name="sequence"
+                    value={formValues['sequence']}
+                    options={[
+                        {value: '4', label: '4 Stream'}, // TODO: Verify these values
+                        {value: '8', label: '8 Stream'},
+                        {value: '0', label: 'other'}
+                    ]}
+                    handleChange={handleInputChange}
+                />
             </SystemComponent>
 
             <SystemComponent height={70}>
