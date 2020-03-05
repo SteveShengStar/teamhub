b99b8803f735ff9cdcfb4a9558711064af0b9a11
zafin-hassan <zafin.hassan@gmail.com>
2020-03-05 18:05:42 -0500

    fixed margins between items in skills for mobile view

--- frontend/components/atoms/AccountSettings/AnchorListItem.js
+++ frontend/components/atoms/AccountSettings/AnchorListItem.js
@@ -1,12 +1,13 @@
-import styled from 'styled-components';
-import Button from '../Button';
+import styled from "styled-components";
+import Button from "../Button";
 
 const AnchorListItem = styled(Button)`
-    padding-top: 2px;
-    padding-bottom: 2px;
+  padding-top: 2px;
+  padding-bottom: 2px;
+  margin-bottom: 4px;
 
-    &:hover {
-        opacity: 1;
-    }
+  &:hover {
+    opacity: 1;
+  }
 `;
 export default AnchorListItem;
--- pages/_app.js
+++ pages/_app.js
@@ -3,76 +3,80 @@
  * Wraps every page around a theme provider
  */
 
-import App from 'next/app';
-import React from 'react';
-import withReduxStore from "../frontend/store/withReduxStore"
+import App from "next/app";
+import React from "react";
+import withReduxStore from "../frontend/store/withReduxStore";
 
-import { persistStore } from 'redux-persist'
-import { PersistGate } from 'redux-persist/integration/react'
-import { Provider } from 'react-redux';
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
 
-import { ThemeProvider } from 'styled-components';
-import theme from '../frontend/components/theme';
+import { ThemeProvider } from "styled-components";
+import theme from "../frontend/components/theme";
 
-import Nav from '../frontend/components/molecules/Nav';
+import Nav from "../frontend/components/molecules/Nav";
 
 const navItems = [
-    {
-        name: 'Explore', link: '/'
-    },
-    {
-        name: 'The Team', link: '/team',
-    },
-    {
-        name: 'Meetings', link: '/meetings'
-    },
-    {
-        name: 'Discover', link: '/discover'
-    },
-    {
-        name: 'Account', link: '/account'
-    }
+  {
+    name: "Explore",
+    link: "/"
+  },
+  {
+    name: "The Team",
+    link: "/team"
+  },
+  {
+    name: "Meetings",
+    link: "/meetings"
+  },
+  {
+    name: "Discover",
+    link: "/discover"
+  },
+  {
+    name: "Account",
+    link: "/account"
+  }
 ];
 
 class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    return {
+      pageProps: Component.getInitialProps
+        ? await Component.getInitialProps(ctx)
+        : {}
+    };
+  }
 
-    static async getInitialProps({ Component, ctx }) {
-        return {
-            pageProps: Component.getInitialProps
-                ? await Component.getInitialProps(ctx)
-                : {}
-        };
+  constructor(props) {
+    super(props);
+    this.persistor = persistStore(props.reduxStore);
+  }
+
+  render() {
+    const { Component, pageProps, router, reduxStore } = this.props;
+
+    let index = -1;
+    if (router && router.route) {
+      const split = router.route.split("/")[1];
+      if (split)
+        index = navItems.findIndex(item => item.link === "/" + split[1]);
+      else index = navItems.findIndex(item => item.link === "/");
     }
-
-    constructor(props) {
-        super(props)
-        this.persistor = persistStore(props.reduxStore)
-    }
-
-    render () {
-        const { Component, pageProps, router, reduxStore } = this.props;
-
-        let index = -1;
-        if (router && router.route) {
-            const split = router.route.split('/')[1];
-            if (split) index = navItems.findIndex(item => item.link === '/' + split[1]);
-            else index = navItems.findIndex(item => item.link === '/');
-        }
-        return (
-            <Provider store={reduxStore}>
-                <ThemeProvider theme={theme}>
-                    <PersistGate
-                        loading={<Component {...pageProps} />}
-                        persistor={this.persistor}
-                    >
-                        <Nav navItems={navItems} index={index}/>
-                        <Component {...pageProps} />
-                    </PersistGate>
-                </ThemeProvider>
-                
-            </Provider>
-        );
-    }
+    return (
+      <Provider store={reduxStore}>
+        <ThemeProvider theme={theme}>
+          <PersistGate
+            loading={<Component {...pageProps} />}
+            persistor={this.persistor}
+          >
+            <Nav navItems={navItems} index={index} />
+            <Component {...pageProps} />
+          </PersistGate>
+        </ThemeProvider>
+      </Provider>
+    );
+  }
 }
 
 export default withReduxStore(MyApp);
