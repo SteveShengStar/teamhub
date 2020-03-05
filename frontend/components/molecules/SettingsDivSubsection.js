78de958d42384c9991615a68388c8db72c7f6b53
zafin-hassan <zafin.hassan@gmail.com>
2020-03-05 15:31:22 -0500

    added margin to top of subsection headings

--- frontend/components/molecules/SettingsDivSubsection.js
+++ frontend/components/molecules/SettingsDivSubsection.js
@@ -1,61 +1,60 @@
-import React from 'react';
-import styled from 'styled-components';
-import {startCase} from 'lodash';
+import React from "react";
+import styled from "styled-components";
+import { startCase } from "lodash";
 
-import Header4 from '../atoms/Header4';
-import AnchorListItem from '../atoms/AccountSettings/AnchorListItem';
-import {SystemComponent} from '../atoms/SystemComponents';
+import Header4 from "../atoms/Header4";
+import AnchorListItem from "../atoms/AccountSettings/AnchorListItem";
+import { SystemComponent } from "../atoms/SystemComponents";
 
-import theme from '../theme'; // TODO: use props=>theme instead of importing
+import theme from "../theme"; // TODO: use props=>theme instead of importing
 
 const CustomHeading = styled(Header4)`
-    font-size: 16.5px;
+  font-size: 16.5px;
+  margin-top: 1%;
 `;
 
-const ListItemWrapper = ({variant, label}) => {
-    return (
-        <AnchorListItem variant={variant} 
-            mr={theme.space[5]}
-        >
-            {label}
-        </AnchorListItem>
-    );
+const ListItemWrapper = ({ variant, label }) => {
+  return (
+    <AnchorListItem variant={variant} mr={theme.space[5]}>
+      {label}
+    </AnchorListItem>
+  );
 };
 
 const SettingsDivSubsection = ({
-                                headerText, 
-                                isLabelListSection = false,
-                                labelValues,
-                                labelStyleVariants,
-                                children}) => {
-    //console.log(theme.colors[subteams[1]]);
+  headerText,
+  isLabelListSection = false,
+  labelValues,
+  labelStyleVariants,
+  children
+}) => {
+  //console.log(theme.colors[subteams[1]]);
 
-    // TODO: revise this condition later
-    // TODO: update the keys later
-    // TODO: make sure labelValues and labelStylingVariants have same length
-    let sectionBody;
-    if (isLabelListSection) {
-        sectionBody = labelValues.map((labelValue, i) => 
-            <ListItemWrapper key={i}
-                variant={labelStyleVariants[i]} 
-                label={startCase(labelValue)}
-            />
-        );
-    } else {
-        sectionBody = children;
-    }
-    
-    return (
-        <>
-            {headerText &&  
-                <SystemComponent height={30}>
-                    <CustomHeading>{headerText}</CustomHeading>
-                </SystemComponent>
-            }
-            <SystemComponent>
-                {sectionBody}
-            </SystemComponent>
-        </>
-    );
-}
+  // TODO: revise this condition later
+  // TODO: update the keys later
+  // TODO: make sure labelValues and labelStylingVariants have same length
+  let sectionBody;
+  if (isLabelListSection) {
+    sectionBody = labelValues.map((labelValue, i) => (
+      <ListItemWrapper
+        key={i}
+        variant={labelStyleVariants[i]}
+        label={startCase(labelValue)}
+      />
+    ));
+  } else {
+    sectionBody = children;
+  }
+
+  return (
+    <>
+      {headerText && (
+        <SystemComponent height={30}>
+          <CustomHeading>{headerText}</CustomHeading>
+        </SystemComponent>
+      )}
+      <SystemComponent>{sectionBody}</SystemComponent>
+    </>
+  );
+};
 export default SettingsDivSubsection;
