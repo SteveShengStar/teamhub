369fe12edd7ece39ce035990625f357ac9e4a471
Steven Xiong <steveshengstar@gmail.com>
2021-01-01 12:59:41 -0500

    Remove the on-campus/off-campus dot

--- frontend/components/molecules/MemberPreviewComponent.js
+++ frontend/components/molecules/MemberPreviewComponent.js
@@ -10,33 +10,19 @@ import Image from '../atoms/Image';
 const GridLayout = styled(SystemComponent)`
     display: grid;
     grid-template-rows: 18px 18px;
-    grid-template-columns: auto 1fr 20px;
+    grid-template-columns: auto 1fr;
     grid-column-gap: ${props => props.theme.space[4]}px;
     padding: ${props => props.theme.space[3]}px;
-    /*box-shadow: ${props => props.theme.shadows.default};*/
     cursor: pointer;
     transition: ${props => props.theme.transitions.default};
     &:hover {
         transform: scale(1.01);
-        /*background-color: white; */
         background-color: ${props => props.theme.colors.greys[1]};
-        /*box-shadow: ${props => props.theme.shadows.default};*/
     }
     &:active {
-        /*box-shadow: ${props => props.theme.shadows.dark};*/
         transform: scale(1.025);
     }
 `;
-
-const Dot = styled.div`
-    align-self: center;
-    justify-self: end;
-    width: 15px;
-    height: 15px;
-    border-radius: 8px;
-    background-color: ${props => props.isOnStream ? "#32E67E" : props.theme.colors.theme};
-    grid-row: 1/3;
-`
 
 const RowFlexLayout = styled(SystemComponent)`
     display: flex;
@@ -66,11 +52,9 @@ const MemberPreviewComponent = ({name, subteam, role, onClick, imageUrl, term, i
             </RowFlexLayout>
 
             <RowFlexLayout gridRow="2/3" gridColumn="2/3">
-                <BorderlessButton variant={'software'}>{subteam && subteam.name || ""}</BorderlessButton>
+                <BorderlessButton variant='software'>{subteam && subteam.name || ""}</BorderlessButton>
                 <Body>{role}</Body>
             </RowFlexLayout>
-            <Dot isOnStream={isOnStream}/>
-
         </GridLayout>
     );
 };
