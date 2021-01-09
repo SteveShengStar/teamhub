b7753d0807928f263dcc2f38cab9c3e9b253a289
Steven Xiong <steveshengstar@gmail.com>
2021-01-08 22:29:50 -0500

    Commenting out slack-related code.

--- pages/api/slack.js
+++ pages/api/slack.js
@@ -1,43 +1,43 @@
-import { WebClient } from "@slack/web-api";
-import { constants } from "../../constants";
+// import { WebClient } from "@slack/web-api";
+// import { constants } from "../../constants";
 
-// Create a new instance of the WebClient class with the token read from your environment variable
-// const web = new WebClient(process.env.REACT_APP_SLACK_KEY);
-const web = new WebClient(constants.SLACK_KEY);
+// // Create a new instance of the WebClient class with the token read from your environment variable
+// // const web = new WebClient(process.env.REACT_APP_SLACK_KEY);
+// const web = new WebClient(constants.SLACK_KEY);
 
-const getAllUsers = async () => {
-  try {
-    const users = await web.users.list({
-      limit: 3 // Can be modified
-    });
-    return users;
-  } catch (error) {
-    console.log(error);
-  }
-}
+// const getAllUsers = async () => {
+//   try {
+//     const users = await web.users.list({
+//       limit: 3 // Can be modified
+//     });
+//     return users;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
-const getUser = async (email) => {
-  try {
-    const user = await web.users.lookupByEmail({
-      email: email
-    });
-    return user;
-  } catch (error) {
-    console.log(error);
-  }
-};
+// const getUser = async (email) => {
+//   try {
+//     const user = await web.users.lookupByEmail({
+//       email: email
+//     });
+//     return user;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
 
-export const getUserId = async (email) => {
-  const user = await getUser(email);
-  return user.user.id;
-}
+// export const getUserId = async (email) => {
+//   const user = await getUser(email);
+//   return user.user.id;
+// }
 
-export const startGroupConversation = async (users) => {
-  try {
-    return await web.conversations.open({
-      users: users
-    })
-  } catch(error) {
-    console.log(error);
-  }
-}
+// export const startGroupConversation = async (users) => {
+//   try {
+//     return await web.conversations.open({
+//       users: users
+//     })
+//   } catch(error) {
+//     console.log(error);
+//   }
+// }
