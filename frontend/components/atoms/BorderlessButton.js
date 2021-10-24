44265edf7af4483faebe44255684a0c1501c471c
Boya <boyazang@hotmail.com>
2021-10-23 22:33:29 -0400

    added software and lim teams

--- frontend/components/atoms/BorderlessButton.js
+++ frontend/components/atoms/BorderlessButton.js
@@ -4,44 +4,50 @@ import { variant } from 'styled-system';
 import Button from './Button';
 
 const BorderlessButton = styled(Button)(
-    {
-        backgroundColor: 'transparent',
-        padding: 0,
+  {
+    backgroundColor: 'transparent',
+    padding: 0,
+  },
+  variant({
+    variants: {
+      primary: {
+        color: 'action',
+      },
+      alert: {
+        color: 'alertAction',
+      },
+      neutral: {
+        color: 'black',
+      },
+      business: {
+        color: 'business',
+      },
+      lim: {
+        color: 'lim',
+      },
+      software: {
+        color: 'software',
+      },
+      mechanical: {
+        color: 'mechanical',
+      },
+      electrical: {
+        color: 'electrical',
+      },
+      admin: {
+        color: 'admin',
+      },
+      exec: {
+        color: 'exec',
+      },
+      infrastructure: {
+        color: 'infrastructure',
+      },
+      web: {
+        color: 'web',
+      },
     },
-    variant({
-        variants: {
-            primary: {
-                color: 'action'
-            },
-            alert: {
-                color: 'alertAction'
-            },
-            neutral: {
-                color: 'black'
-            },
-            software: {
-                color: 'software'
-            },
-            mechanical: {
-                color: 'mechanical',
-            },
-            electrical: {
-                color: 'electrical'
-            },
-            admin: {
-                color: 'admin',
-            },
-            exec: {
-                color: 'exec'
-            },
-            infrastructure: {
-                color: 'infrastructure'
-            },
-            web: {
-                color: 'web'
-            }
-        }
-    })
+  })
 );
 
 export default BorderlessButton;
