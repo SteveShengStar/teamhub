b5a5f7e5f49d6950bcfd41fcac5302609255e5dd
unknown <evanmatt.wong@gmail.com>
2021-10-25 20:19:58 -0400

    made changes according to feedback

--- pages/api/members/update.js
+++ pages/api/members/update.js
@@ -1,15 +1,12 @@
-// atm, this route is meant to update all members with a new parameter (of whatever)
-
+// Route is meant to add a new parameter to all members
+// e.g, if req.body was { active: true }, then it add an active: true to all members in the db
 const data = require("../../../backend/data/index.js");
 module.exports = async (req, res) => {
   await data.initIfNotStarted();
   if (req.method === "POST") {
-
     const authStatus = await data.auth.checkAnyUser(req.headers['authorization'], res);
     if (authStatus) {
         res.setHeader('Content-Type', 'application/json');
-        console.log(req.body);
-
         if (await data.util.checkIsEmptyBody(req.body)) {
             res.statusCode = 400;
             res.end(JSON.stringify(await data.util.resWrapper(async () => {
@@ -17,17 +14,14 @@ module.exports = async (req, res) => {
             })));
             return;
         }
-
         res.statusCode = 200;
         res.end(JSON.stringify(await data.util.resWrapper(async () => {
           return await data.members.updateAllMembers({$set: req.body });
-
         })));
     }
-    else {
-      res.statusCode = 404;
-      res.end();
   }
-
-  }
+  else {
+    res.statusCode = 404;
+    res.end();
+}
 };
