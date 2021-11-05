82357c10a6c29acb74e89c50df907ead5300145d
unknown <evanmatt.wong@gmail.com>
2021-11-04 22:28:08 -0400

    on create new member, active status is automatically true

--- backend/data/handlers/auth.js
+++ backend/data/handlers/auth.js
@@ -98,6 +98,7 @@ auth.login = async (tokenObj) => {
                     },
                     email: payload['email'],
                     imageUrl: payload['picture'],
+                    active: true,
                     token,
                     tokenExpiry: Date.now() + (1000 * 60 * 60 * 24 * 7)
                 });
