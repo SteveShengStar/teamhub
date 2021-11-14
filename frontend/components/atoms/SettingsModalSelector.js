3e12e89a6d86440d9dcce59d160d3bd875035321
Steven Xiong <steveshengstar@gmail.com>
2021-11-14 14:18:28 -0500

    Fixed Bugs.

--- frontend/components/atoms/SettingsModalSelector.js
+++ frontend/components/atoms/SettingsModalSelector.js
@@ -3,12 +3,12 @@ import EditProfileModal from '../organisms/EditProfileModal';
 import EditTeamsModal from '../organisms/EditTeamsModal';
 import EditLinksModal from '../organisms/EditLinksModal';
 
-const SettingsModalSelector = ({isLoaded, activeModal, handleCloseModal}) => {
+const SettingsModalSelector = ({userDataLoaded, activeModal, handleCloseModal}) => {
     return (
         <>
-            <EditProfileModal dataLoaded={isLoaded} handleCloseModal={handleCloseModal} visible={activeModal === ACTIVE_MODAL.PROFILE_INFO} />
-            <EditTeamsModal dataLoaded={isLoaded} handleCloseModal={handleCloseModal} visible={activeModal === ACTIVE_MODAL.TEAMS_RESPONSIBILITIES}/>
-            <EditLinksModal dataLoaded={isLoaded} handleCloseModal={handleCloseModal} visible={activeModal === ACTIVE_MODAL.EXTERNAL_LINKS}/>
+            <EditProfileModal dataLoaded={userDataLoaded} handleCloseModal={handleCloseModal} visible={activeModal === ACTIVE_MODAL.PROFILE_INFO} />
+            <EditTeamsModal dataLoaded={userDataLoaded} handleCloseModal={handleCloseModal} visible={activeModal === ACTIVE_MODAL.TEAMS_RESPONSIBILITIES}/>
+            <EditLinksModal dataLoaded={userDataLoaded} handleCloseModal={handleCloseModal} visible={activeModal === ACTIVE_MODAL.EXTERNAL_LINKS}/>
         </>
     )
 }
--- pages/settings/links.js
+++ pages/settings/links.js
@@ -40,7 +40,7 @@ const AddInfoPrompt = styled(SystemLink)`
 const LinksSection = ({sectionTitle, setActiveModal, userDataLoaded}) => {
 
     const { user } = useSelector(state => state.userState);
-    const links = userDataLoaded && user ? user.links : [];
+    const links = userDataLoaded && user && user.links ? user.links : [];
     const linkLabelIcons = ['fa-globe', 'fa-linkedin', 'fa-github', 'fa-facebook-square'] // CSS Class names of font-awesome icons
     const accountTypes = ["website", 'linkedin', 'github', 'facebook'];
     const altText = {
