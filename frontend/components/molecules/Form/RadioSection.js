8d20938c321cf5283e0f99ce275ac10c2ac4e792
aditisandhu <71997372+aditis10@users.noreply.github.com>
2022-02-11 21:56:27 -0500

    updated radio buttons and checkboxes

--- frontend/components/molecules/Form/RadioSection.js
+++ frontend/components/molecules/Form/RadioSection.js
@@ -2,14 +2,19 @@ import React, { useState, useContext, useEffect, useRef } from 'react';
 import { SystemComponent, SystemSpan } from '../../atoms/SystemComponents';
 import RadioButton from '../../atoms/Radio';
 
-const RadioSection = ({options}) => {
-    const [selectedOption, setSelectedOption] = useState(undefined);
+const RadioSection = ({options, name, selectedOption, setSelectedOption}) => {
+
+    const selectOption = (newSelectedValue) => {
+        setSelectedOption(
+            name, newSelectedValue
+        );
+    }
 
     return (
         <>
             {
                 options.map(opt => 
-                    <SystemComponent onClick={() => setSelectedOption(opt)} height="40px" >
+                    <SystemComponent onClick={() => selectOption(opt)} height="40px" >
                         <RadioButton selected={opt === selectedOption} />
                         <SystemComponent ml="45px" fontSize="16px" pt="2px">{opt}</SystemComponent>
                     </SystemComponent>
