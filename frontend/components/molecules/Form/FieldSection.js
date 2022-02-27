f645166d4a9eb94c30b5ee25a35e03e707a042a4
aditisandhu <71997372+aditis10@users.noreply.github.com>
2022-02-27 18:50:19 -0500

    fix mobile display view (duplicate commit)

--- frontend/components/molecules/Form/FieldSection.js
+++ frontend/components/molecules/Form/FieldSection.js
@@ -42,7 +42,7 @@ const FieldSection = ({title, description='', type="textbox", required, onChange
                         theme.textInputHeight.small, 
                         theme.textInputHeight.medium, 
                         theme.textInputHeight.large]} 
-                        width={["90%", "90%", "600px"]}
+                        width="98%"
                         name={name}
                         value={value}
                         onChange={(e) => onChange(name, e.target.value)}
@@ -80,7 +80,8 @@ const FieldSection = ({title, description='', type="textbox", required, onChange
     return (
         <SystemComponent
             fontSize={theme.fontSizes.header3}
-            textAlign='center'
+            textAlign='left'
+            width={["98%", "100%", "100%", "600px"]}
         >
             <SystemComponent textAlign='left' mb={["10px", "15px"]}>
                 <TitleSection text={title} required={required} />
