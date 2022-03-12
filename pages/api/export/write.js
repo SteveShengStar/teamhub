b2f007d86487644814a1b71b449d6fdcf543451f
Steven Xiong <steveshengstar@gmail.com>
2022-03-11 21:06:35 -0500

    Added Google Sheets API Integration.

---
+++ pages/api/export/write.js
@@ -0,0 +1,30 @@
+const data = require('../../../backend/data/index');
+const cookie = require('cookie');
+
+module.exports = async (req, res) => {
+    await data.initIfNotStarted();
+    if (req.method === 'POST') {
+        
+        // Get the Access Token from the request headers
+        // const token = cookie.parse(req.headers.cookie).token;
+        // const authStatus = await data.auth.checkAnyUser(`Bearer ${token}`, res);
+                            
+        if (true) {
+            const token = req.headers['authorization'].split(' ')[1];
+            console.log(token);
+            res.setHeader('Content-Type', 'application/json');
+
+            res.statusCode = 200;
+            
+            res.end(JSON.stringify(await data.util.resWrapper(async () => {
+                return await data.exportsheet.write(token);
+            })));
+        } else {
+            res.statusCode = 401;
+            res.end();
+        }
+    } else {
+        res.statusCode = 404;
+        res.end();
+    }
+};
