ce5d22ee20a6596d1b785937c12cdce3d9e46407
Steven Xiong <steveshengstar@gmail.com>
2022-04-23 23:48:26 -0400

    Make user fill in the registration form only once.

--- frontend/hooks/useShouldRedirect.js
+++ frontend/hooks/useShouldRedirect.js
@@ -6,27 +6,28 @@ import { USER_REGISTRATION_REQUIRED_FORM_FIELDS } from '../constants';
  * @param {*} user
  * @param {*} router
  */
-const useShouldRedirect = (user, router) => {
-    if (!user || !user._id) {
-         // Redirect user to the initial login/signup page
-        if (router.pathname != "/login") {
-            router.push("/login")
-            return true
+const useShouldRedirect = (user, router, isExistingUser) => {
+    if (!isExistingUser) {
+        if (!user || !user._id) {
+            // Redirect user to the initial login/signup page
+            if (router.pathname != "/login") {
+                router.push("/login")
+                return true
+            }
+            return false
         }
-        return false
+
+        const userFields = Object.keys(user);
+        if (USER_REGISTRATION_REQUIRED_FORM_FIELDS.some(field => !userFields.includes(field))) { // TODO: add all the necessary checks, to handle page refresh
+            // Redirect user to the 2nd step of the sign-up process
+            if (router.pathname != "/login/register") {
+                router.push("/login/register")
+                return true
+            }
+            return false
+        }
     }
 
-    const userFields = Object.keys(user);
-    if (USER_REGISTRATION_REQUIRED_FORM_FIELDS.some(field => !userFields.includes(field))) { // TODO: add all the necessary checks, to handle page refresh
-        // Redirect user to the 2nd step of the sign-up process
-        if (router.pathname != "/login/register") {
-            router.push("/login/register")
-            return true
-        }
-        return false
-    }
-    
-    // TODO: is this needed ?
     if (router.pathname.startsWith("/login")) {
         // If user entered all required information already, then redirect the user to landing page.
         router.push("/");
--- pages/api/auth/logout.js
+++ pages/api/auth/logout.js
@@ -6,7 +6,7 @@ export default async (req, res) => {
     if (req.method === 'POST') {
         // Get the Access Token from the request headers
         const token = cookie.parse(req.headers.cookie).token;
-        const authStatus = await data.auth.checkAnyUser(`Bearer ${token}`, res);        
+        const authStatus = await data.auth.checkAnyUser(`Bearer ${token}`, res, true);        
         if (authStatus) {
             res.setHeader('Content-Type', 'application/json');
             
