62ffd1b280dc8430559ba0d95b74c15dee183416
G-Wang12 <gordonwang0608@gmail.com>
2022-06-17 22:13:15 -0400

    Instead of using font awesome, svg and jpg were used.

--- pages/form/settings.js
+++ pages/form/settings.js
@@ -10,15 +10,18 @@ import Card from '../../frontend/components/atoms/Card';
 import Button from '../../frontend/components/atoms/Button';
 
 const EditFormButton = styled(Button)`
-    width: 200px;
-    margin-bottom: ${props => props.theme.space.cardPaddingSmall}px; 
+    height: 48px;
+    width: 400px;
+    margin-bottom: ${props => props.theme.space.headerBottomMargin}px; 
     // ^^^ to fit the figma layout this margin-bottom should actually be 16px instead of 10px i think.
     margin-top: 16px;
-    // background: #4C6DFF;
+    border-radius: 5px;
 `;
 
 const ExportRespButton = styled(Button)`
-    width: 200px;
+    width: 400px;
+    height: 48px;
+    border-radius: 5px;
 `
 
 const CheckmarkRow = styled.div`
@@ -26,22 +29,14 @@ const CheckmarkRow = styled.div`
     align-items: flex-start;
     margin-top: 5px;
     margin-left: 4px;
+    margin-bottom: 24px; 
 `
 const Text = styled.div`
-    // font-size: ${props => props.theme.fontSizes.header4}px;
-    // line-height: 2;
-    // position: absolute;
-    // width: 344px;
-    // height: 72px;
-    // left: 104px;
-    // top: 396px;
-
     font-family: 'SF Pro';
     font-style: normal;
     font-weight: 400;
     font-size: 20px;
     line-height: 24px;
-    margin-bottom: 24px; 
 
     color: #000000;
 
@@ -58,28 +53,26 @@ const BigIconWrapper = styled.div`
     justify-content: center;
     box-sizing: border-box;
 `
+const Svg = styled.img`
+    justify-content: center;
+    height: 30px;
+    width: 30px;
+    margin-top: 10px;
+`
 
 const IconWrapper = styled.div`
-    // margin-top: 5px;
-    // margin-right: 5px;
     margin-right: 16px;
-    // margin-left: 4px;
     display: flex;
     align-item: flex-start;
-    // border-radius: 50%;
-    // border: 1px solid;
-    // padding: 2px;
     height: 24px;
     width: 24px;
 `
 
 const TitleText = styled.div`
-    // font-size: ${props => props.theme.fontSizes.header2}px;
-    // font-size: 30px;
     font-family: 'SF Pro';
     font-style: normal;
     font-weight: 700;
-    font-size: 24px;
+    font-size: ${props => props.theme.fontSizes.header2}px;
     line-height: 29px;
     /* identical to box height */
 
@@ -90,18 +83,19 @@ const TitleText = styled.div`
 
     color: #000000;
 `
-const SpecialIcon = styled.div`
-    font-family: 'Avenir Next Condensed';
-    font-style: normal;
-    font-weight: 600;
-    font-size: 20px;
-    line-height: 18px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`
+// const SpecialIcon = styled.div`
+//     font-family: 'Avenir Next Condensed';
+//     font-style: normal;
+//     font-weight: 600;
+//     font-size: 20px;
+//     line-height: 18px;
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     margin-top: 20px;
+// `
 
-const FormMetadataSection = ({faClassName, title, descripBullets}) => {
+const FormMetadataSection = ({src, title, descripBullets}) => {
     return (
         <Card
             display="flex"
@@ -111,7 +105,7 @@ const FormMetadataSection = ({faClassName, title, descripBullets}) => {
         >
             <SystemComponent>
                 <BigIconWrapper>
-                    {title === "Beginning of Term Form" ? <SpecialIcon>New</SpecialIcon> : <i className={"fa " + faClassName}/>}
+                    <Svg src = {src}></Svg>
                 </BigIconWrapper>
             </SystemComponent>
             <SystemComponent>
@@ -133,7 +127,6 @@ const FormMetadataSection = ({faClassName, title, descripBullets}) => {
                     </CheckmarkRow>
                     )
                         
-                    // <li key={i}>{bullet}</li>
                 })}
             </SystemComponent>
             <EditFormButton>Edit Form</EditFormButton>
@@ -153,19 +146,22 @@ const Settings = () => {
                 gridRowGap={6}
             >
                 <FormMetadataSection
-                    faClassName='fa-light fa-user-plus fa-3x'
+                    // faClassName='fa-light fa-user-plus fa-2x'
+                    src = "/static/returning-members-icon.jpg"
                     title="Returning Members Form"
                     descripBullets={["Filled in at the end of the term", "Notify team leads about whether the member wants to continue on Waterloop next term"]}
                 />
                 <FormMetadataSection 
-                    faClassName='fa-envelope-o'
+                    // faClassName='fa-envelope-o'
+                    src = "/static/beginning-of-term-icon.jpg"
                     title="Beginning of Term Form"
                     descripBullets={["Filled in at the beginning of the term", "Notify team leads about a member's plans for this term"]}
 
                 />
                 <FormMetadataSection
-                    faClassName='fa-light fa-user-plus fa-3x' 
-                    title="New Member Form"
+                    // faClassName='fa-light fa-user-plus fa-3x' 
+                    src = "/static/sign-up-icon.svg"
+                    title="Sign-Up Form"
                     descripBullets={["Filled in only once", "Every user will fill out when logging in to TeamHub"]}
                 />
             </SystemComponent>
