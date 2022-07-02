7ffce840bc9f6999d5fd26af7f1aacd9e46966b3
Steven Xiong <steveshengstar@gmail.com>
2022-07-02 01:06:11 -0400

    Add Required indicator in form section. Made duplicate section go after duplicated section.

--- frontend/components/organisms/formsection/Section.js
+++ frontend/components/organisms/formsection/Section.js
@@ -2,24 +2,24 @@ import React from 'react';
 import Selectable from './Selectable';
 import TextSection from './TextSection';
 
-const Section = ({type, name, question, helpText, options, canDelete, handleTypeChange, handleInputChange, handleOptionChange, handleOptionAdd, handleOptionDelete, handleSectionDelete, handleSectionDuplicate}) => {
+const Section = ({type, name, question, helpText, options, required, canDelete, handleTypeChange, handleInputChange, handleOptionChange, handleOptionAdd, handleOptionDelete, handleSectionDelete, handleSectionDuplicate, handleToggleRequired}) => {
     switch(type) {
         case "text":
         case "email":
         case "longtext":
         case "numbers":
         case "phone":
-            return <TextSection sectionName={name} type={type} question={question} helpText={helpText} handleTypeChange={handleTypeChange} handleInputChange={handleInputChange}
-                     handleSectionDelete={handleSectionDelete} handleSectionDuplicate={handleSectionDuplicate} canDelete={canDelete} />
+            return <TextSection sectionName={name} type={type} question={question} helpText={helpText} required={required} handleTypeChange={handleTypeChange} handleInputChange={handleInputChange}
+                     handleSectionDelete={handleSectionDelete} handleSectionDuplicate={handleSectionDuplicate} handleToggleRequired={handleToggleRequired} canDelete={canDelete} />
         case "checkbox": 
         case "radio": 
         case "boolean": 
         case "menu_single":
         case "menu_multi":
-            return <Selectable sectionName={name} type={type} question={question} helpText={helpText} options={options}
+            return <Selectable sectionName={name} type={type} question={question} helpText={helpText} options={options} required={required}
                         handleTypeChange={handleTypeChange} handleInputChange={handleInputChange} 
                         handleOptionChange={handleOptionChange} handleOptionAdd={handleOptionAdd} handleOptionDelete={handleOptionDelete} 
-                        handleSectionDelete={handleSectionDelete} handleSectionDuplicate={handleSectionDuplicate} canDelete={canDelete}
+                        handleSectionDelete={handleSectionDelete} handleSectionDuplicate={handleSectionDuplicate} handleToggleRequired= {handleToggleRequired} canDelete={canDelete}
                     />
         default:
             return <></>;
