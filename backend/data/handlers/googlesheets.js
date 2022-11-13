278be6f5da57551d24430b6f5ddd4852472027d5
Steven Xiong <steveshengstar@gmail.com>
2022-11-13 13:10:06 -0500

    Export more info for the Start of Term form responses.

--- backend/data/handlers/googlesheets.js
+++ backend/data/handlers/googlesheets.js
@@ -31,9 +31,8 @@ googlesheets.readfile = async (token) => {
 googlesheets.writefile = async (token, formName) => {
     switch (formName) {
         case 'register':
-            return exportRegister(token);
         case 'startofterm':
-            return exportStartTerm(token);
+            return exportRegister(token);
         case 'returning':
             return exportReturning(token);
     }
@@ -69,50 +68,6 @@ const exportRegister = async (token) => {
     const fileName = `Waterloop Roster - ${currentDate.toLocaleString('en-CA', {
         timeZone: 'EST',
     })}`;
-    const fileMetadata = {
-        name: fileName,
-        parents: [],
-        mimeType: 'application/vnd.google-apps.spreadsheet',
-    };
-    const driveRequest = {
-        resource: fileMetadata,
-        fields: 'id',
-    };
-    const googleDrive = getGoogleDriveClient(token);
-    const driveResponse = await googleDrive.files.create(driveRequest);
-
-    // update spreadsheet
-    const request = {
-        spreadsheetId: driveResponse.data.id,
-        range: 'Sheet1',
-        valueInputOption: 'USER_ENTERED',
-        resource: {
-            values: spreadsheetData,
-        },
-    };
-    const googleSheets = getGoogleSheetsClient(token);
-    const response = await googleSheets.spreadsheets.values.update(request);
-    return response.config.url;
-};
-
-const exportStartTerm = async (token) => {
-    const fields = getFields(START_TERM_FIELDS);
-    const userData = (await getAll(fields)).map((row) => {
-        return {
-            fullName: row.name.first + ' ' + row.name.last,
-            phoneNumber: row.miscDetails?.phone ?? '',
-        };
-    });
-
-    const spreadsheetData = [START_TERM_SPREADSHEET_HEADERS];
-    spreadsheetData.push(...userData.map((data) => Object.values(data)));
-
-    //create new file with Drive api
-    const currentDate = new Date();
-    const fileName = `Beginning of Term Form - ${currentDate.toLocaleString(
-        'en-CA',
-        { timeZone: 'EST' }
-    )}`;
     const fileMetadata = {
         name: fileName,
         parents: [],
